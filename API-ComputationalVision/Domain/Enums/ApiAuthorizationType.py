4913596f0acb2e5fcc36c14d4ae4147cef2a22be
Caio <caiombaraujo@gmail.com>
2025-09-28 15:38:16 -0300

    removing deprecated api

--- API-ComputationalVision/Domain/Enums/ApiAuthorizationType.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-
-class ApiAuthorizationType(str, Enum):
-    BASIC = "Basic"
-    BEARER = "Bearer"
--- API-ComputationalVision/Domain/Models/ApplicationConfigurationModels/ApiModels/AuthenticationHeaderModel.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-from Domain.Enums.ApiAuthorizationType import ApiAuthorizationType
-
-
-class AuthenticationHeaderModel(BaseModel):
-    TYPE: ApiAuthorizationType
-    AUTHORIZATION: str
